4db3d051619e748284435669857d5f2756665099
Sakshi Dwivedi <sakshidwivedi406@gmail.com>
2025-10-25 18:47:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-blue-500'>Sakshi</h1>
+      <h1 className='text-purple-500'>I am a developer</h1>
+    </>
+  )
+}
+
+export default App
